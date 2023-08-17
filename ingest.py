7a8f207011f48b049d558d9d3068a75f747d6cfb
seif9116 <smetwall@ualberta.ca>
2023-08-17 14:35:14 -0600

    added the functionality to output a .ics file

--- ingest.py
+++ ingest.py
@@ -8,8 +8,7 @@ from langchain.vectorstores import Chroma
 from langchain.llms import OpenAI
 
 os.environ['OPENAI_API_KEY'] = getpass.getpass('OpenAi API Key:')
-llm = OpenAI()
-
+openai.api_key = os.environ['OPENAI_API_KEY']
 file_path = os.path.expanduser('~/university/bachelor_3/fall/FIN301/syllabus.pdf')
 loader = PyPDFLoader(file_path)
 text_splitter = RecursiveCharacterTextSplitter(
@@ -24,14 +23,18 @@ print('creating vector store...')
 db = Chroma.from_documents(docs, OpenAIEmbeddings())
 
 query = "What are the dates, times and locations for 1) Midterms, Quizzes, Final Exams, 2) Assignments, 3) Classes"
-docs = db.similarity_search(query, 3)
-for doc in docs:
-    print(doc.page_content)
+docs = db.similarity_search(query, k=6)
 
 def create_ics(docs):
-    init_ics = "use the following information to create .ics for my Midterms, Quizzes, Final Exams, Assignments, and Classes"
+    init_ics = "use the following information to create .ics for my 1) Midterms, Quizzes, Final Exams 2) Assignments, and 3) Classes \n"
     for doc in docs:
         init_ics += doc.page_content
-    return llm(init_ics)
+    print(init_ics)
+    return openai.ChatCompletion.create(
+            model='gpt-3.5-turbo',
+            messages=[
+                    {'role': 'user', 'content': init_ics}
+                ]
+            )
 
-print(create_ics(docs))
+print(create_ics(docs)['choices'][0]['message']['content'])
